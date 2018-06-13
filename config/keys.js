9f795a9b8da1d194266540133ee8a522ca7bdbfc
Jugal Joshi <jkj.619@gmail.com>
2018-06-13 20:37:26 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Jugal%20Joshi:Pujara%408@ds247270.mlab.com:47270/merntest'
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
+
+const dbURI = require('./config/keys').mongoURI;
+mongoose.connect(dbURI).then(() => {
+    console.log('db connected');
+}).catch((err) => console.log(err));
+app.get('/', (req, res) => {
+    res.send('Hello156756');
+})
+
+
+//api routes
+app.use('api/users', users);
+app.use('api/posts', posts);
+app.use('api/profile', profile);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`server running on ${port}`);
+})
